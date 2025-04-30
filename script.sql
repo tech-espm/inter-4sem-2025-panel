59556c11b5370365dd382f08e2fcf8b549cf4bac
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2025-04-30 12:12:19 -0300

    Criação das queries de consolidação

--- script.sql
+++ script.sql
@@ -177,3 +177,54 @@ CREATE TABLE passagem (
   KEY passagem_data_id_sensor (data, id_sensor),
   KEY passagem_id_sensor (id_sensor)
 );
+
+-- Queries de consolidação
+
+-- Query de consolidação de ocupação máxima por zona por dia do mês e por hora, para o heatmap de visão explodida por dia do mês com N colunas e 24 linhas
+-- Para preencher os dias/horas faltantes:
+-- Se o primeiro registro do dia não for às 00:00 preencher com 0 todas as horas das 00:00 até a primeira retornada
+-- Os registros faltantes ao longo do dia devem ser preenchidos com o valor do campo ultimo_pessoas do último registro retornado
+select tmp.id_sensor, tmp.dia, tmp.hora, tmp.pessoas, u.pessoas ultimo_pessoas from
+(
+	select id_sensor, date_format(date(data), '%d/%m/%Y') dia, extract(hour from data) hora, max(pessoas) pessoas, max(id) id_ultimo
+	from pca
+	where data between '2025-03-03 00:00:00' and '2025-03-14 23:59:59'
+	group by id_sensor, dia, hora
+	order by id_sensor, dia, hora
+) tmp
+inner join pca u on u.id = tmp.id_ultimo;
+
+select date_format(date(data), '%d/%m/%Y') dia, extract(hour from data) hora, avg(luminosidade) luminosidade, avg(umidade) umidade, avg(voc) voc, avg(co2) co2, avg(pressao_ar) pressao_ar, avg(ruido) ruido, avg(ponto_orvalho) ponto_orvalho
+from creative
+where data between '2025-03-03 00:00:00' and '2025-03-14 23:59:59'
+group by dia, hora
+order by dia, hora;
+
+select date_format(date(data), '%d/%m/%Y') dia, extract(hour from data) hora, avg(condutividade) condutividade, avg(umidade) umidade, avg(temperatura) temperatura
+from solo
+where data between '2025-03-03 00:00:00' and '2025-03-14 23:59:59'
+group by dia, hora
+order by dia, hora;
+
+select date_format(date(data), '%d/%m/%Y') dia, extract(hour from data) hora, avg(bateria) bateria, avg(h2s) h2s, avg(umidade) umidade, avg(nh3) nh3, avg(temperatura) temperatura
+from odor
+where id_sensor = 1 and data between '2025-03-03 00:00:00' and '2025-03-14 23:59:59'
+group by dia, hora
+order by dia, hora;
+
+select id_sensor, date(data) dia, avg(delta) presenca_media
+from presenca
+where data between '2025-03-10 00:00:00' and '2025-03-14 23:59:59' and ocupado = 0
+group by id_sensor, dia
+order by id_sensor, dia;
+
+select date_format(date(data), '%d/%m/%Y') dia, extract(hour from data) hora, avg(umidade) umidade, avg(temperatura) temperatura
+from temperatura
+where id_sensor = 1 and data between '2025-03-03 00:00:00' and '2025-03-14 23:59:59'
+group by dia, hora
+order by dia, hora;
+
+select date_format(date(data), '%d/%m/%Y') dia, extract(hour from data) hora, sum(entrada) entrada, sum(saida) saida
+from passagem
+where id_sensor = 2 and data between '2025-03-03 00:00:00' and '2025-03-14 23:59:59'
+group by dia, hora;
